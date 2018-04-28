9398d93c1002ce6c455184c4a6b5b2f7ddf25a05
Syed Afzal <syed.afzal@codenvoi.com>
2018-04-29 02:28:23 +0500

    delete api done

--- server/server.js
+++ server/server.js
@@ -52,6 +52,18 @@ app.get('/todos/:id', (req,res)=>{
         .catch(e=>res.status(400).send())
 });
 
+app.delete('/todos/:id', (req,res) => {
+    const id = req.params.id;
+
+    Todo.findByIdAndRemove(id).then((todo) => {
+
+        if(!todo){
+            return res.status(404).send()
+        }
+        res.status(200).send({todo})
+    }).catch(e=>res.status(400).send())
+});
+
 app.listen(port, ()=>{
     console.log("server is up on port : ", port);
 });
